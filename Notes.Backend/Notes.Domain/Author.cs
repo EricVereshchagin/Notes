7ae6666e45434161f39c780a407031a01786bca4
Eric Vereshchagin <Vereshchagin.Eric@yandex.ru>
2022-02-13 20:46:12 +0300

    creating Domain level

---
+++ Notes.Backend/Notes.Domain/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Drawing;
+
+namespace Notes.Domain
+{
+    public class Author
+    {
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+        public Image Photo { get; set; }
+    }
+}
---
+++ Notes.Backend/Notes.Domain/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Notes.Domain
+{
+    public class Note
+    {
+        public Author Сreator { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+}
