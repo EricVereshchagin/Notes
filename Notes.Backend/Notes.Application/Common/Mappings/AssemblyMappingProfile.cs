9c9df520737db39ac031cda4170dab326d48496a
Eric Vereshchagin <Vereshchagin.Eric@yandex.ru>
2022-02-14 17:09:34 +0300

    Add AutoMapper & usage logic mapping

---
+++ Notes.Backend/Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assemby) => ApplyMappingFromAssembly(assemby);
+
+        private List<Type> SearchTypesForMapping(Assembly assembly) => assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                .Any(i => i.IsGenericType &&
+                i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+        private void ApplyMappingFromAssembly(Assembly assembly)
+        {
+            var types = SearchTypesForMapping(assembly);
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
